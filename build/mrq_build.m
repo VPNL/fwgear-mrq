e0fb10082ccc4e63cbc585784fd70e8dab600956
Michael Perry <lmperry@stanford.edu>
2019-04-05 12:08:33 -0700

    build code

---
+++ build/mrq_build.m
@@ -0,0 +1,49 @@
+% This script should be run on Matlab 2017b, GLNXA64.
+%
+% EXAMPLE USAGE
+%       /software/matlab/r2017b/bin/matlab -nodesktop -r mrq_build
+
+% Check that we are running a compatible version
+if (isempty(strfind(version, '9.3.0'))) || (isempty(strfind(computer, 'GLNXA64')))
+    error('You must compile this function using R2017b (9.3.0.713579) 64-bit (glnxa64). You are using %s, %s', version, computer);
+end
+
+disp(mfilename('fullpath'));
+compileDir = fileparts(mfilename('fullpath'));
+if ~strcmpi(pwd, compileDir)
+    disp('You must run this code from %s', compileDir);
+end
+
+% Check for spm8
+spm_path = fileparts(which('spm'))
+if isempty(spm_path)
+  error('Please add the SPM8 to your path prior to building this code!')
+end
+
+% Download the source code
+disp('Cloning source code...');
+system('mkdir source_code')
+system('git clone https://github.com/vistalab/vistasoft source_code/vistasoft');
+system('git clone https://github.com/gllmflndn/JSONio.git source_code/JSONio');
+system('git clone https://github.com/xiangruili/dicm2nii.git source_code/dcm2nii');
+system('git clone https://github.com/mezera/mrQ.git source_code/mrQ');
+system('git clone https://github.com/kendrickkay/knkutils.git source_code/knkutils');
+system('wget https://github.com/flywheel-io/core/releases/download/5.0.4/flywheel-matlab-sdk-5.0.4.zip -O source_code/flywheel-matlab-sdk.zip && unzip source_code/flywheel-matlab-sdk.zip && rm -f source_code/flywheel-matlab-sdk.zip');
+
+
+% Set paths
+disp('Adding paths to build scope...');
+restoredefaultpath;
+addpath(genpath(spm_path));
+addpath(genpath(fullfile(pwd, 'source_code')));
+
+% Compile
+disp('Running compile code...');
+mcc -v -R -nodisplay -m ../../fwgear_mrq.m -d ./bin
+
+% Clean up
+disp('Cleaning up...')
+rmdir(fullfile(pwd, 'source_code'), 's');
+
+disp('Done!');
+exit
